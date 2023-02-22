e7010f57811accb048c1f184fffcfa4e0048e36d
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2023-02-22 23:45:07 +0100

    ADD: Search functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -27,49 +27,58 @@ const API_KEY = "939fbadf",
   API_ID = "tt3896198";
 
 function getRandomSearchQuery() {
-  const DEFAULT_SEARCH_QUERIES = ["smart", "developer", "technology", "adventure", "ceo"];
+  const DEFAULT_SEARCH_QUERIES = ["smart", "developer", "technology", "adventure", "ceo", "africa"];
   const randomIndex = Math.floor(Math.random() * DEFAULT_SEARCH_QUERIES.length);
 
   return DEFAULT_SEARCH_QUERIES[randomIndex];
 }
 
 function Movies() {
+  const [isLoading, setIsLoading] = useState(true);
   const [search, setSearch] = useState(getRandomSearchQuery());
   const [featuredLoaded, setFeaturedLoaded] = useState(true);
   const [movies, setMovies] = useState([]);
   const [parsedMovies, setParsedMovies] = useState(movies);
-  const shouldLoad = useRef(true);
 
   useEffect(() => {
     async function loadFeatured() {
+      setIsLoading(true);
       const result = await fetch(
-        `https://www.omdbapi.com/?apikey=${API_KEY}&i=${API_ID}&s={search}`
+        `https://www.omdbapi.com/?apikey=${API_KEY}&i=${API_ID}&s=${search}`
       );
       const resultJson = await result.json();
       setFeaturedLoaded(true);
       setParsedMovies(resultJson.Search);
+      setIsLoading(false);
     }
 
-    shouldLoad.current && loadFeatured();
-
-    return () => {
-      shouldLoad.current = false;
-    };
+    loadFeatured();
   }, [search]);
+
+  function setSearchQuery(e) {
+    e.preventDefault();
+    if (!e.target.search.value) return;
+    setSearch(e.target.search.value);
+  }
+
+  if (isLoading) return <Loading />;
 
   return (
     <Wrapper>
-      <div class="input-group mb-5">
-        <input
-          type="search"
-          className="form-control"
-          placeholder="Search movies"
-          aria-label="Search movies"
-        />
-        <span class="input-group-text">
-          <i className="bi bi-search"></i>
-        </span>
-      </div>
+      <form onSubmit={setSearchQuery}>
+        <div className="input-group mb-5">
+          <input
+            type="search"
+            name="search"
+            className="form-control"
+            placeholder="Search movies"
+            aria-label="Search movies"
+          />
+          <span className="input-group-text">
+            <i className="bi bi-search"></i>
+          </span>
+        </div>
+      </form>
 
       <div className="movies | d-grid gap-4 pb-5">
         {parsedMovies?.length
@@ -134,6 +143,14 @@ function MovieCard({ movie }) {
           <small>{movie.Year}</small>
         </p>
       </div>
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="d-flex height-80 align-items-center justify-content-center">
+      <div className="loading"></div>
     </div>
   );
 }
